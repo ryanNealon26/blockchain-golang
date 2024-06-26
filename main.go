5c52b355576904c4fd8d74470a483e55538d13cb
RyanNealon2 <rwn9303@gmail.com>
2024-06-26 12:34:24 -0400

    Added API support to blockchain

--- main.go
+++ main.go
@@ -3,18 +3,21 @@ package main
 import (
 	"crypto/sha256"
 	"fmt"
+	"net/http"
 	"time"
+
+	"github.com/gin-gonic/gin"
 )
 
 type Block struct {
-	hash      string
-	data      string
-	prevHash  string
-	timeStamp string
+	Hash      string
+	Data      string
+	PrevHash  string
+	TimeStamp string
 }
 
 type BlockChain struct {
-	chain []Block
+	Chain []Block
 }
 
 func createHash(data string) string {
@@ -31,46 +34,71 @@ func createHash(data string) string {
 func addBlock(chain *BlockChain, data string) {
 	timeStamp := time.Now().Format(time.RFC850)
 	block := Block{createHash(data), data, getPrevHash(*chain), timeStamp}
-	chain.chain = append(chain.chain, block)
+	chain.Chain = append(chain.Chain, block)
 }
 func getPrevHash(chain BlockChain) string {
-	size := len(chain.chain)
-	return chain.chain[size-1].hash
+	size := len(chain.Chain)
+	return chain.Chain[size-1].Hash
 }
 func initChain() BlockChain {
 	var chain BlockChain
 	data := "Genesis Block"
 	timeStamp := time.Now().Format(time.RFC850)
 	genesis := Block{createHash(data), data, "", timeStamp}
-	chain.chain = append(chain.chain, genesis)
+	chain.Chain = append(chain.Chain, genesis)
 	return chain
 
 }
 func isChainValid(chain BlockChain) bool {
-	currentBlock := chain.chain
+	currentBlock := chain.Chain
 	for i := 1; i < len(currentBlock); i++ {
 		next := currentBlock[i]
-		if next.prevHash != currentBlock[i-1].hash {
+		if next.PrevHash != currentBlock[i-1].Hash {
 			return false
 		}
 
 	}
 	return true
 }
+
+var chain = initChain()
+
+func getBlockChain(c *gin.Context) {
+	if isChainValid(chain) {
+		c.IndentedJSON(http.StatusOK, chain.Chain)
+	} else {
+		c.IndentedJSON(http.StatusOK, "Error Found in blockchain")
+	}
+}
+func postBlock(c *gin.Context) {
+	var data struct {
+		Data string `json:"data"`
+	}
+	if err := c.BindJSON(&data); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	addBlock(&chain, data.Data)
+	c.JSON(http.StatusOK, gin.H{"message": "Block added successfully", "data": data.Data})
+
+}
 func main() {
-	chain := initChain()
+	router := gin.Default()
+	router.GET("/blockchain", getBlockChain)
+	router.POST("/blockchain", postBlock)
 	addBlock(&chain, "First Block After Genesis")
 	addBlock(&chain, "Second Block After Genesis")
 	if isChainValid(chain) {
 		fmt.Println("Chain is valid")
-		for i := 0; i < len(chain.chain); i++ {
+		for i := 0; i < len(chain.Chain); i++ {
 			fmt.Println("Block #", i+1)
-			fmt.Println("	Block Data: ", chain.chain[i].data)
-			fmt.Println("	Previous Hash: ", chain.chain[i].prevHash)
-			fmt.Println("	Current Hash: ", chain.chain[i].hash)
-			fmt.Println("	Time Stamp: ", chain.chain[i].timeStamp)
+			fmt.Println("	Block Data: ", chain.Chain[i].Data)
+			fmt.Println("	Previous Hash: ", chain.Chain[i].PrevHash)
+			fmt.Println("	Current Hash: ", chain.Chain[i].Hash)
+			fmt.Println("	Time Stamp: ", chain.Chain[i].TimeStamp)
 		}
 	} else {
 		fmt.Println("Execution halted, invalid block in chain")
 	}
+	router.Run("localhost:8080")
 }
